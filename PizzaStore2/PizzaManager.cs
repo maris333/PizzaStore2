2f8a45eb4023385266827ed60a2dd4e051ebdee2
maris333 <animaral@gmail.com>
2023-05-28 17:01:59 +0200

    Refactor

---
+++ PizzaStore2/PizzaManager.cs
@@ -0,0 +1,29 @@
+﻿namespace PizzaStore2
+{
+    internal class PizzaManager
+    {
+        public void BuyPizza()
+        {
+            Console.WriteLine("Choose margherita or pepperoni pizza:");
+            var input = Console.ReadLine();
+
+            switch (input)
+            {
+                case "margherita":
+                    PizzaCreator margheritaCreator = new MargheritaPizzaCreator();
+                    IPizza margheritaPizza = margheritaCreator.CreatePizza();
+                    margheritaPizza.BuyPizza();
+                    break;
+                case "pepperoni":
+                    PizzaCreator pepperoniCreator = new PepperoniPizzaCreator();
+                    IPizza pepperoniPizza = pepperoniCreator.CreatePizza();
+                    pepperoniPizza.BuyPizza();
+                    break;
+                default:
+                    Console.WriteLine("Invalid choice.");
+                    Environment.Exit(0);
+                    break;
+            }
+        }
+    }
+}
